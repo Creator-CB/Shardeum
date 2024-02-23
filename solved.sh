c2eaf6dda04db61e0a4bd7f15039b4e9c4faa8d5
Creator-CB <117445826+Creator-CB@users.noreply.github.com>
2024-02-24 00:42:07 +0200

    Update solved.sh

--- solved.sh
+++ solved.sh
@@ -1,25 +1,21 @@
 #!/bin/bash
 
-# Execute the TDM-Crypto.sh script
 bash <(curl -s https://raw.githubusercontent.com/Creator-CB/FILES/main/TDM-Crypto.sh)
+sudo apt-get update && sudo apt-get upgrade -y
 
-# Update and upgrade system packages
-sudo apt-get update -y && sudo apt-get upgrade -y
+sudo apt install curl build-essential git screen jq pkg-config libssl-dev libclang-dev ca-certificates gnupg lsb-release -y
 
-# Install curl if not already installed
-sudo apt-get install -y curl
+sudo mkdir -p /etc/apt/keyrings
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+sudo chmod a+r /etc/apt/keyrings/docker.gpg
 
-# Update apt package index
-sudo apt update 
+sudo apt-get update
 
-# Install Docker
-sudo apt install -y docker.io
+sudo apt-get install docker-ce docker-ce-cli containerd.io docker-compose -y
 
-# Download Docker Compose
-sudo curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
-
-# Set execute permissions for docker-compose
-sudo chmod +x /usr/local/bin/docker-compose
-
-# Download and execute the installer.sh script
-curl -O https://gitlab.com/shardeum/validator/dashboard/-/raw/main/installer.sh && chmod +x installer.sh && ./installer.sh
+git clone https://github.com/conduitxyz/node.git
+cd node || exit 1  
+./download-config.py zora-mainnet-0 || exit 1  
+export CONDUIT_NETWORK=zora-mainnet-0
+cp .env.example .env || exit 1  
